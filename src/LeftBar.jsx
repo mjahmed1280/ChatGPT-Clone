7f1b9aa2ce4dc2b0e8271eebe79376999c535ba9
mjahmed1280@gmail.com <Mj1280mj1280@>
2023-10-24 01:47:25 +0530

    modified:   src/LeftBar.jsx

--- src/LeftBar.jsx
+++ src/LeftBar.jsx
@@ -1,48 +1,62 @@
-import React from 'react'
-import {Button, ButtonGroup, Card,ListGroup, Form} from 'react-bootstrap';
+import React from 'react';
+import { Button, ButtonGroup, Card, ListGroup } from 'react-bootstrap';
 import { FaBeer, FaUser } from 'react-icons/fa';
-import { TbLetterJ } from "react-icons/tb";
+import { FaGithub } from 'react-icons/fa'; // Import the GitHub icon
 import { useMyContext } from './MyContext';
 
 function LeftBar() {
-
     const { toggleButton } = useMyContext();
 
-  return (
-    <div style={{display:"flex", flexDirection:"column"}}>
-        <ButtonGroup className='w-100'>
-            <Button variant='outline-dark'className='w-100 m-1' onClick={ toggleButton }>+ New Chat</Button>
-            <Button variant='outline-dark'className='w-50 m-1'>#</Button>
-        </ButtonGroup>
-        <div>
-        </div>
-        <div>
-        <Card style={{  }}>
-        <ListGroup variant="dark">
-            <ListGroup.Item>Chat History</ListGroup.Item>
-            <ListGroup.Item>Chat History </ListGroup.Item>
-            <ListGroup.Item>Chat History</ListGroup.Item>
-            <ListGroup.Item>Chat History</ListGroup.Item>
-            <ListGroup.Item>Chat History</ListGroup.Item>
-            <ListGroup.Item>Chat History</ListGroup.Item>
-        </ListGroup>
-        </Card>
-        </div>
+    const openGitHubRepo = () => {
+        window.open("https://github.com/mjahmed1280/ChatGPT-Clone", "_blank");
+    };
 
-        <div style={{marginTop:"80%"}}>        
-            <Card>
-            <Card.Body>
-                <Card.Text><Button variant='outline-dark' className='w-100'>Upgrdae to Plus</Button></Card.Text>
-                 <Card.Text> <Button variant='outline-dark' className=''> <TbLetterJ /> </Button>     Jack Ahmed</Card.Text>
-            
-            </Card.Body>
-            </Card>
+    const openGitHubProfile = () => {
+        window.open("https://github.com/mjahmed1280", "_blank");
+    };
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <ButtonGroup className='w-100'>
+                <Button variant='outline-dark' className='w-100 m-1' onClick={toggleButton}>+ New Chat</Button>
+                <Button variant='outline-dark' className='w-50 m-1' onClick={openGitHubProfile}>
+                    <FaGithub /> {/* Use the GitHub icon here */}
+                </Button>
+            </ButtonGroup>
+            <div>
+            </div>
+            <div>
+                <Card style={{}}>
+                    <ListGroup variant="dark">
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                        <ListGroup.Item>Chat History</ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </div>
+
+            <div style={{ marginTop: "80%" }}>
+                <Card>
+                    <Card.Body>
+                        <Card.Text>
+                            <Button variant='outline-dark' className='w-100' onClick={openGitHubRepo}>
+                            GitHub Repository
+                            </Button>
+                        </Card.Text>
+                        <Card.Text>
+                            <Button variant='outline-dark' className='' onClick={openGitHubProfile}>
+                                <FaGithub /> {/* Use the GitHub icon here */}
+                                Jakaria Ahmed
+                            </Button>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
-
-    </div>
-
-    
-  )
+    );
 }
 
-export default LeftBar
+export default LeftBar;
